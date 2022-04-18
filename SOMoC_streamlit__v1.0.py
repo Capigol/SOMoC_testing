4fbb41e1efe8d17b1e3ddc2c865ac16c809948bb
Lucas Nicolas Alberca <86059300+Capigol@users.noreply.github.com>
2022-04-18 15:06:00 -0300

    Update SOMoC_streamlit__v1.0.py

--- SOMoC_streamlit__v1.0.py
+++ SOMoC_streamlit__v1.0.py
@@ -135,13 +135,13 @@ def Get_input_data():
 
     if input_file is not None:
         name = input_file.name
-        data1 = pd.read_csv(input_file, delimiter=',', header=None)
+        data1 = pd.read_csv(input_file, delimiter=',', header=True)
         st.write(data1)
         if "SMILES" in list(data1.columns):
             data = data1["SMILES"]
-            
         else:
-            data = data1.iloc[:, 0]     
+            data = pd.read_csv(input_file, delimiter=',', header=None)
+            #data = data1.iloc[:, 0]     
       
     else:
         name = Get_name("test/focal_adhesion.csv")
